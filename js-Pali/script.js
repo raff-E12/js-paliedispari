4d0dea4e385245fe114f4a86d35084e6f951536a
Raff_12 <raffelecoppola381@gmail.com>
2025-02-28 17:48:25 +0100

    feat: Correzzione è disposizione degli script

--- js-Pali/script.js
+++ js-Pali/script.js
@@ -3,31 +3,17 @@
 
 //Definire le variabili per l'operazioni
 let input_user_par = window.prompt('Inserisci una parola a piacere:', '');
-const list_words = ['Pane', 'Farfalla', 'Spazio', 'Meme', 'Shrek', 'Palindroma'];
-let index = 0;
-let condition = true;
 
 //Creazione della funzione di convalidazione
 
 function Words_Ver(input) {
-   while (index < list_words.length && condition === true) {
-      if (list_words[index] === 'Palindroma') {
-        if (input === 'Palindroma') {
-            input = window.alert('Complimenti, hai vinto la sfida di oggi.');
-            console.log('Complimenti, hai vinto la sfida di oggi.');
-        } else {
-            input = window.alert('Mi dispiace, non hai vinto la sfida di oggi.');
-            console.log('Mi dispiace, non hai vinto la sfida di oggi.');
-        }
-        condition = false;
-        break
-      }
-      index++;
-   }
-   if (condition === false) {
-    input = window.alert('Hai esaurito il tentativo, ricarica la pagina per riprovare.')
-    console.log('Hai esaurito il tentativo, ricarica la pagina per riprovare.');
-   }
+  if (input === 'Palindroma') {
+    input = window.alert('La parola che hai scelto è correttamente corrispondente a quello inserito');
+    console.log('La parola che hai scelto è correttamente corrispondente a quello inserito');
+  } else {
+    input = window.alert('La parola che hai scelto non è correttamente corrispondente a quello inserito');
+    console.log('La parola che hai scelto non è correttamente corrispondente a quello inserito');
+  }
 }
 
 //Implementazione della funzione
