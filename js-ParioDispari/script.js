c8078e1f98dccf7b700d2f5eb1887517e499bdd7
Raff_12 <raffelecoppola381@gmail.com>
2025-02-28 17:22:09 +0100

    feat: Correzione del secondo esercizio sulle relative task

--- js-ParioDispari/script.js
+++ js-ParioDispari/script.js
@@ -2,12 +2,61 @@
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto
+// Dichiariamo chi ha vinto.
 
-//Definizione delle Variabili per il processo
+//Definizione delle Variabili per il processo.
 
 let input_window_user = window.prompt('Inserisci Pari o Dispari:', '');
-let Number_ver_input = Number(window.prompt('Inserisci un numero da 1 a 5:', ''));
+let number_user_input = Number(window.prompt('Inserisci un numero da 1 a 5:', ''));
+let operation_number = 0;
+let Number_user = '';
+let computer_number = 0;
+let result_number = 0;
 
-//Definire la Funzione per la task
 
+//Funzione di generativa per il computer.
+
+function Number_random(){
+    return Math.ceil(Math.random() * 5) + 1;
+}
+
+//Funzione di verifica sul numero scelto dall'utente.
+
+function Method_result(user, number){
+    if (user === 'Pari') {
+        if (!isNaN(number) && number % 2 === 0) {
+            console.log('Il numero che hai scelto è Pari.');
+        } else{
+            console.log('Il numero che hai scelto non è Pari.');
+        }
+      } else if(user === 'Dispari') {
+        if (!isNaN(number) && number % 2 !== 0) {
+            console.log('Il numero che hai scelto è Dispari.');
+        } else{
+            console.log('Il numero che hai scelto non è Dispari.');
+        }
+      } else{
+        console.log('Non hai inserito correttamente i valori di input, ricarica e riprova');
+      }
+  }
+
+  Method_result(input_window_user, number_user_input);
+
+  //Operazione procedurale sul risultato finale.
+  computer_number = Number_random();
+  operation_number = Math.ceil(Math.random() * isNaN(number_user_input)) + 1;
+  result_number = operation_number + computer_number;
+
+ //Funzione di confronto sul risultato ottenuto per stabilire il vincitore. 
+function Result_Op(result) {
+    if (result % 2 === 0) {
+        window.alert("Ha vinto l'utente, Complimenti.");
+        console.log("Ha vinto l'utente, Complimenti.");
+      } else {
+        window.alert("Ha vinto il computer, Hai Perso.");
+        console.log("Ha vinto il computer, Hai Perso.")
+      }
+    
+}
+
+Result_Op(result_number);
